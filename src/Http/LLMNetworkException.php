5de8516001604331827c2cfa7691380916a7c4be
rkr <ron.kirschler@gmail.com>
2025-12-24 19:01:34 +0100

    Improve `LLMNetworkException` by parsing error messages from JSON response content using `JsonException` handling.

--- src/Http/LLMNetworkException.php
+++ src/Http/LLMNetworkException.php
@@ -2,6 +2,7 @@
 
 namespace DvTeam\ChatGPT\Http;
 
+use JsonException;
 use Throwable;
 
 class LLMNetworkException extends LLMException {
@@ -9,6 +10,13 @@ class LLMNetworkException extends LLMException {
 	 * @param array<string, string[]> $headers
 	 */
 	public function __construct(public string $contents, public array $headers, int $statusCode, Throwable $previous = null) {
-		parent::__construct('', $statusCode, $previous);
+		$message = '';
+		try {
+			/** @var object{error?: object{message?: string}} $data */
+			$data = json_decode($contents, associative: false, depth: 10, flags: JSON_THROW_ON_ERROR);
+			$message = $data->error->message ?? '';
+		} catch (JsonException) {
+		}
+		parent::__construct($message, $statusCode, $previous);
 	}
 }
