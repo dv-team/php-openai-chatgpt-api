d06937542bbe60b26c7e6ca0390685154742836f
rkr <ron.kirschler@gmail.com>
2026-01-09 11:28:43 +0100

    Add `toInputContentParts` method to `ChatAttachment` interface to define input content serialization structure

--- src/Messages/ChatAttachment.php
+++ src/Messages/ChatAttachment.php
@@ -3,4 +3,10 @@
 namespace DvTeam\ChatGPT\Messages;
 
 interface ChatAttachment {
+	/**
+	 * Maps the attachment to the Responses API input schema message content items.
+	 *
+	 * @return list<array<string, mixed>>
+	 */
+	public function toInputContentParts(): array;
 }
