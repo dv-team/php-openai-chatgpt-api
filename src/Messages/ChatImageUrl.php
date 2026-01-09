75f46032b0fa37346daf877d3fc7281270ba18f9
rkr <ron.kirschler@gmail.com>
2026-01-09 11:28:43 +0100

    Add `toInputContentParts` method to `ChatImageUrl` to support input image content serialization

--- src/Messages/ChatImageUrl.php
+++ src/Messages/ChatImageUrl.php
@@ -4,4 +4,11 @@ namespace DvTeam\ChatGPT\Messages;
 
 class ChatImageUrl implements ChatAttachment {
 	public function __construct(public string $url) {}
+
+	public function toInputContentParts(): array {
+		return [[
+			'type' => 'input_image',
+			'image_url' => $this->url,
+		]];
+	}
 }
